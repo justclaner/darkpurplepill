ca83ece4315174e3f3cd14bdfebb6c13e236e283
= <justclaner@gmail.com>
2025-06-24 13:35:50 -0400

    frontend boilerplate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Routes>
+      
+      <div>App</div>
+    </Routes>
+  )
+}
+
+export default App
